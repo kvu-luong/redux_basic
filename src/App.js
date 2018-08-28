72d7ddf94eb9a0f95264490844c687a004d9a693
Luong Khanh Vu <intern-03@vn.devinition.com>
2018-08-28 17:59:17 +0700

    using store

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+
+
+  render() {
+
+    var redux = require("redux");
+    let oldState = {
+      num: ["man hinh", "chuot", "desktop"],
+      editStatus : true,
+    }
+    var reducer1 = (state = oldState, action) =>{
+      switch(action.type){
+        case "CHANGE_STATUS":
+            return {...oldState, editStatus: !state.editStatus }
+            break;
+        case "ADD_NEW":
+            return {...oldState, num: [...state.num, action.newItem]}
+            break;
+        default:
+            return "hello world";
+            break;
+      }
+    }
+    let store1 = redux.createStore(reducer1);
+    console.log(store1.getState());
+    store1.dispatch({type: "CHANGE_STATUS"});
+    console.log(store1.getState());
+    store1.dispatch({type: "ADD_NEW", newItem: "lion"});
+    console.log(store1.getState());
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
+    );
+   
+  }
+
+  
+}
+
+export default App;
